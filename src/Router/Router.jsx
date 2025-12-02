0ddddcd612492c3aa13fa59630f89e9d31e1e0e7
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2025-12-02 22:38:56 +0600

    Create login and Register page design

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main/Main";
 import Home from "../Pages/Home/Home/Home";
+import Register from "../shared/Register/Register";
+import Login from "../shared/Login/Login";
 
 export const router = createBrowserRouter([
   {
@@ -10,7 +12,13 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
-      },
+      },{
+          path:"/register",
+          element:<Register></Register>
+        },{
+          path:"/login",
+          element: <Login></Login>
+        }
     ],
   },
 ]);
